1d70cbdc7ff9f93cb69e1ec06259baac4f878f84
qhy040404 <qhy040404@163.com>
2024-02-06 22:23:52 +0800

    Update pages

--- app/utils/dgp.py
+++ app/utils/dgp.py
@@ -2,7 +2,7 @@ import httpx
 
 
 def fetch_token_from_homa(username: str, encoded_password: str) -> str:
-    url = "https://homa.com/api/token"
-    data = {"username": username, "password": encoded_password}
-    response = httpx.post(url, data=data)
-    return response.json()["access_token"]
+  url = "https://homa.com/api/token"
+  data = {"username": username, "password": encoded_password}
+  response = httpx.post(url, data=data)
+  return response.json()["access_token"]
