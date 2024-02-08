89a4405c81c6f8929d0d08ff931836fca196cc5e
qhy040404 <qhy040404@163.com>
2024-02-08 21:06:35 +0800

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from fastapi.responses import RedirectResponse
-import urllib.parse
 
 from app.config import HOSTNAME
 
@@ -11,6 +10,5 @@ router = APIRouter(prefix="/users", tags=["users"])
 @router.get("/login", response_class=RedirectResponse)
 async def login_with_github(token: str):
 	response = RedirectResponse(url="/dashboard.html", status_code=302)
-	cookie_value = urllib.parse.quote(f"Bearer {token}")
-	response.set_cookie(key="token", value=cookie_value, max_age=60 * 60 * 3, domain=HOSTNAME)
+	response.set_cookie(key="homa_token", value=token, max_age=60 * 60 * 3, domain=HOSTNAME)
 	return response
