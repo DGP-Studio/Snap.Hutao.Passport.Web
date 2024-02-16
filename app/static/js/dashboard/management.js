173776371f93055fcd0af536ba52d7180de06a83
qhy040404 <qhy040404@163.com>
2024-02-16 23:15:10 +0800

    add confirmation

--- app/static/js/dashboard/management.js
+++ app/static/js/dashboard/management.js
@@ -1,44 +1,58 @@
 function compensateHutaoCloud() {
 	let days = document.getElementById("management-compensation-days-input").value
 
-	fetch(`https://homa.snapgenshin.com/Service/GachaLog/Compensation?days=${days}`, {
-		headers: {
-			"Authorization": BearerWrap(getToken())
-		}
-	}).then(response => response.json())
-		.then(data => {
-			if (data.retcode === 0) {
-				alert("操作成功，" + data.message);
-			} else {
-				alert("操作失败：" + data.message);
+	if (!days) {
+		alert("Please input valid days");
+		return;
+	}
+
+	if (confirm(`Confirm to compensate ${days} days?`)) {
+		fetch(`https://homa.snapgenshin.com/Service/GachaLog/Compensation?days=${days}`, {
+			headers: {
+				"Authorization": BearerWrap(getToken())
 			}
-		})
-		.catch(error => {
-			console.error("Error:", error);
-			alert("操作失败：" + error);
-		});
+		}).then(response => response.json())
+			.then(data => {
+				if (data.retcode === 0) {
+					alert("Success, " + data.message);
+				} else {
+					alert("Failed, " + data.message);
+				}
+			})
+			.catch(error => {
+				console.error("Error:", error);
+				alert("Error, " + error);
+			});
+	}
 }
 
 function designateHutaoCloud() {
 	let username = document.getElementById("management-designation-user-input").value
 	let days = document.getElementById("management-designation-days-input").value
 
-	fetch(`https://homa.snapgenshin.com/Service/GachaLog/Designation?userName=${username}&days=${days}`, {
-		headers: {
-			"Authorization": BearerWrap(getToken())
-		}
-	}).then(response => response.json())
-		.then(data => {
-			if (data.retcode === 0) {
-				alert("操作成功，" + data.message);
-			} else {
-				alert("操作失败：" + data.message);
+	if (!username || !days) {
+		alert("Please input valid username and days");
+		return;
+	}
+
+	if (confirm(`Confirm to designate ${username} for ${days} days?`)) {
+		fetch(`https://homa.snapgenshin.com/Service/GachaLog/Designation?userName=${username}&days=${days}`, {
+			headers: {
+				"Authorization": BearerWrap(getToken())
 			}
-		})
-		.catch(error => {
-			console.error("Error:", error);
-			alert("操作失败：" + error);
-		});
+		}).then(response => response.json())
+			.then(data => {
+				if (data.retcode === 0) {
+					alert("Success, " + data.message);
+				} else {
+					alert("Failed, " + data.message);
+				}
+			})
+			.catch(error => {
+				console.error("Error:", error);
+				alert("Error, " + error);
+			});
+	}
 }
 
 function uploadAnn() {
@@ -48,31 +62,52 @@ function uploadAnn() {
 	let content = document.getElementById("management-ann-content").value
 	let severity = document.getElementById("management-ann-severity").value
 
-	let data = {
-		"Title": title,
-		"Link": link,
-		"MaxPresentVersion": maxVersion,
-		"Content": content,
-		"Severity": parseInt(severity)
+	let data = {}
+
+	if (title) {
+		data["Title"] = title
 	}
 
-	fetch("https://homa.snapgenshin.com/Service/Announcement/Upload", {
-		method: "POST",
-		headers: {
-			"Authorization": BearerWrap(getToken()),
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify(data)
-	}).then(response => response.json())
-		.then(data => {
-			if (data.retcode === 0) {
-				alert("操作成功");
-			} else {
-				alert("操作失败：" + data.message);
-			}
-		})
-		.catch(error => {
-			console.error("Error:", error);
-			alert("操作失败：" + error);
-		});
+	if (link) {
+		data["Link"] = link
+	}
+
+	if (maxVersion) {
+		data["MaxPresentVersion"] = maxVersion
+	}
+
+	if (content) {
+		data["Content"] = content
+	}
+
+	if (severity) {
+		data["Severity"] = parseInt(severity)
+	}
+
+	if (maxVersion && !/^\d+\.\d+\.\d+\.\d+$/.test(maxVersion)) {
+		alert("Please input valid version. e.g. 1.9.5.0");
+		return;
+	}
+
+	if (confirm(`Confirm to upload announcement?\nUpload body: ${JSON.stringify(data)}`)) {
+		fetch("https://homa.snapgenshin.com/Service/Announcement/Upload", {
+			method: "POST",
+			headers: {
+				"Authorization": BearerWrap(getToken()),
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(data)
+		}).then(response => response.json())
+			.then(data => {
+				if (data.retcode === 0) {
+					alert("Success, " + data.message);
+				} else {
+					alert("Failed, " + data.message);
+				}
+			})
+			.catch(error => {
+				console.error("Error:", error);
+				alert("Error, " + error);
+			});
+	}
 }
