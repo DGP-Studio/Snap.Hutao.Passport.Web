ba0eb45b2a9fd496a8adf4efd21edce211c13fca
Masterain <i@irain.in>
2024-02-17 04:20:54 -0800

    add redemption code generate endpoint
    Resolve #3

--- app/routers/redemption.py
+++ app/routers/redemption.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, Depends, Security, Response
+from fastapi import APIRouter, Depends, Security, Response, Request
 from fastapi.security import HTTPAuthorizationCredentials
 from pydantic import BaseModel
+import uuid
 
 from app.mysql_app import crud
 from app.mysql_app.database import SessionLocal
@@ -129,4 +130,60 @@ async def use_redemption_code(code: UseRedemptionCodeRequest, response: Response
     response_body = StandardResponse()
     response_body.retcode = response.status_code == 500
     response_body.message = "Failed to use redemption code. Contact admin. "
+    return response_body
+
+
+def generate_uuid_code() -> str:
+    # Generate a 16-character redemption code
+    return str(uuid.uuid4())[:18].upper()
+
+
+@router.post("/generate", response_model=StandardResponse)
+async def generate_redemption_code(request: Request, response: Response,
+                                   db: SessionLocal = Depends(get_db),
+                                   passport_security: HTTPAuthorizationCredentials = Security(passport_api_key),
+                                   homa_security: HTTPAuthorizationCredentials = Security(homa_bearer)):
+    # Start of identity verification
+    homa_identity = auth_is_homa_token(homa_security)
+    if homa_identity.IsMaintainer:
+        CREATED_BY = f"Homa/{homa_identity.NormalizedUserName}"
+    else:
+        passport_identity = auth_is_passport_token(db, passport_security)
+        if passport_identity.Authority:
+            CREATED_BY = f"Passport/{passport_identity.Authority}"
+        else:
+            response_body = StandardResponse()
+            response_body.retcode = response.status_code == 401
+            response_body.message = "Permission denied."
+            return response_body
+    print(f"Start process redemption code generation: Authority -> {CREATED_BY}")
+
+    request = await request.json()
+    try:
+        number_of_code = int(request.get("n", None))
+        value = int(request.get("v", None))
+        description = request.get("desc", None)
+    except (ValueError, TypeError):
+        response_body = StandardResponse()
+        response_body.retcode = response.status_code == 400
+        response_body.message = "Invalid request parameters."
+        return response_body
+
+    number_of_success = 0
+    added_codes = []
+    while number_of_success < number_of_code:
+        code = generate_uuid_code()
+        code_validation = crud.validate_redemption_code(db, code)
+        if code_validation:
+            # Code already exists
+            continue
+        this_code = RedemptionCode(code=code, value=value, description=description, created_by=CREATED_BY)
+        commit_result = crud.add_redemption_code(db, this_code)
+        if commit_result:
+            number_of_success += 1
+            added_codes.append(code)
+
+    response_body = StandardResponse()
+    response_body.message = "Redemption code generated successfully."
+    response_body.data = {"generated_code": added_codes}
     return response_body
