71396a1fe668f22082f92b3e94b7d3a1ba974ccd
qhy040404 <45379733+qhy040404@users.noreply.github.com>
2024-02-19 20:05:34 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import env_result
 from app.routers import user, token, redemption
 
@@ -9,7 +10,23 @@ api_app = FastAPI(title="Hutao Passport API", openapi_url="/openapi.json")
 api_app.include_router(user.router)
 api_app.include_router(token.router)
 api_app.include_router(redemption.router)
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "http://localhost:8080",
+]
+
+api_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.mount("/api", api_app, name="Hutao Passport API")
+
 
 # Mount the static files directory
 app.mount("/", StaticFiles(directory="./app/static/", html=True), name="static")
